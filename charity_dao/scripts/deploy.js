f425299f106841aaeda04e7097adfc68e8ee9574
thanhkhon03 <2121050666@student.humg.edu.vn>
2025-09-05 13:43:01 +0700

    add fs to record module file

--- charity_dao/scripts/deploy.js
+++ charity_dao/scripts/deploy.js
@@ -1,4 +1,6 @@
 const { ethers } = require("hardhat");
+const fs = require("fs");
+const path = require ("path");
 
 async function main(){
     const [deployer] = await ethers.getSigners();
@@ -11,11 +13,31 @@ async function main(){
     const vault = await CharityVault.deploy(deployer.address);  
 
     //với ethers v6
-    await vault.waitForDeployed();
+    await vault.waitForDeployment();
 
-    console.log("CharityVault deployed to: ", await vault.getAddress());
+    const contractAddress = await vault.getAddress();
+    console.log("CharityVault deployed to: ", contractAddress);
 }
+    
+    //Ghi địa chỉ contract vào file JSON
+    const deploymentsDir = path.join (__dirname, "..", "deployments");
+    if (!fs.existsSync(deploymentsDir)) {
+        fs.mkdirSync(deploymentsDir);
+    }
 
+    const deploymentInfo = {
+        CharityVault: contractAddress,
+        network: "localhost",
+        deployer: deployer.address,
+    };
+
+    fs.writeFileSync(
+        path.join(deploymentsDir, "deployed-address.json"),
+        JSON.stringify(deploymentInfo, null, 2),
+        "utf-8"
+    );
+
+    console.log("Deployment info saved to deployments/deployed-address.json");
 main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
